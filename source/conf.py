246b5177717481c16bbb308846035719b35e9744
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-08-24 16:54:44 -0700

    Include init with doc?

--- source/conf.py
+++ source/conf.py
@@ -39,6 +39,8 @@ extensions = [
     'sphinx.ext.doctest',
 ]
 
+napoleon_include_init_with_doc = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
